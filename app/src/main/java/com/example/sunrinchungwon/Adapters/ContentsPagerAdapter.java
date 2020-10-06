d391c33b37ae73047beb54b5052b7b1c1359b1cd
kimdonguke <rlaehddnr100@gmail.com>
2020-10-07 00:53:19 +0900

    tablayout

---
+++ app/src/main/java/com/example/sunrinchungwon/Adapters/ContentsPagerAdapter.java
@@ -0,0 +1,91 @@
+package com.example.sunrinchungwon.Adapters;
+
+import android.support.v4.app.Fragment;
+
+import android.support.v4.app.FragmentManager;
+
+import android.support.v4.app.FragmentStatePagerAdapter;
+
+
+import com.example.sunrinchungwon.Fragments.Example2Fragment;
+import com.example.sunrinchungwon.Fragments.Example3Fragment;
+import com.example.sunrinchungwon.Fragments.Example4Fragment;
+import com.example.sunrinchungwon.Fragments.Example5Fragment;
+import com.example.sunrinchungwon.Fragments.ExampleFragment;
+
+public class ContentsPagerAdapter extends FragmentStatePagerAdapter {
+
+    private int mPageCount;
+
+
+
+    public ContentsPagerAdapter(FragmentManager fm, int pageCount) {
+
+        super(fm);
+
+        this.mPageCount = pageCount;
+
+    }
+
+
+
+    @Override
+
+    public Fragment getItem(int position) {
+
+        switch (position) {
+
+            case 0:
+
+                ExampleFragment exampleFragment = new ExampleFragment();
+
+                return exampleFragment;
+            case 1:
+
+                Example2Fragment example2Fragment = new Example2Fragment();
+
+                return example2Fragment;
+
+
+
+            case 2:
+
+                Example3Fragment example3Fragment = new Example3Fragment();
+
+                return example3Fragment;
+
+
+
+            case 3:
+
+                Example4Fragment example4Fragment = new Example4Fragment();
+
+                return example4Fragment;
+
+
+
+            case 4:
+
+                Example5Fragment example5Fragment = new Example5Fragment();
+
+                return example5Fragment;
+
+
+
+            default:
+
+                return null;
+
+        }
+
+    }
+
+
+
+    @Override
+
+    public int getCount() {
+        return mPageCount;
+    }
+
+}
--- app/src/main/java/com/example/sunrinchungwon/MainActivity.java
+++ app/src/main/java/com/example/sunrinchungwon/MainActivity.java
@@ -1,13 +1,72 @@
 package com.example.sunrinchungwon;
 
+import android.content.Context;
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.TextView;
+
+import com.example.sunrinchungwon.Adapters.ContentsPagerAdapter;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TabLayout mTabLayout;
+    private Context mContext;
+    private ViewPager mViewPager;
+    private ContentsPagerAdapter mContentPagerAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mContext = getApplicationContext();
+        mTabLayout = (TabLayout) findViewById(R.id.layout_tab);
+        mViewPager = (ViewPager) findViewById(R.id.pager_content);
+
+        mTabLayout.addTab(mTabLayout.newTab().setCustomView(createTabView("홈")));
+
+        mTabLayout.addTab(mTabLayout.newTab().setCustomView(createTabView("게임")));
+
+        mTabLayout.addTab(mTabLayout.newTab().setCustomView(createTabView("영화")));
+
+        mTabLayout.addTab(mTabLayout.newTab().setCustomView(createTabView("도서")));
+
+        mTabLayout.addTab(mTabLayout.newTab().setCustomView(createTabView("뉴스스탠드")));
+
+        mContentPagerAdapter = new ContentsPagerAdapter(
+
+                getSupportFragmentManager(), mTabLayout.getTabCount());
+
+        mViewPager.setAdapter(mContentPagerAdapter);
+        mViewPager.addOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(mTabLayout));
+
+        mTabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                mViewPager.setCurrentItem(tab.getPosition());
+            }
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+                // ㅎ...흥 오버라이드 하던가 말던가!
+            }
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+                // ㅎ...흥 오버라이드 하던가 말던가!
+            }
+        });
+    }
+
+    private View createTabView(String tabName) {
+
+        View tabView = LayoutInflater.from(mContext).inflate(R.layout.custom_tab, null);
+
+        TextView txt_name = (TextView) tabView.findViewById(R.id.txt_name);
+
+        txt_name.setText(tabName);
+
+        return tabView;
     }
 }
